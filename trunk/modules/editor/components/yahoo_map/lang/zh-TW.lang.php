7f51aa9f643b2333f30d862ba6084b9ff6c5c3ce
royallin11 <royallin11@d0f3c0e8-0e7a-11df-96d9-85c6b80b8d9c>
2010-02-02 02:04:49 +0000

    Add/Update TW language

---
+++ trunk/modules/editor/components/yahoo_map/lang/zh-TW.lang.php
@@ -0,0 +1,26 @@
+<?php
+    /**
+     * @file   /modules/editor/components/yahoo_map/lang/zh-TW.lang.php
+     * @author translator: royallin
+     * @brief  編輯器模組 > Yahoo 地圖元件正體中文語言
+     **/
+
+    $lang->width = "寬度";
+    $lang->height = "高度";
+    $lang->yahoo_map_preview = "預覽";
+    $lang->about_width = "像素(px)<br />*只能輸入數字";
+    $lang->about_height = "像素(px)<br />*只能輸入數字";
+
+    // Expressions
+    $lang->about_address = "例) 倫敦, 巴黎";
+    $lang->about_address_use = "請按 [插入] 按鈕。然後地圖將會被新增到文章中。";
+
+    // 錯誤訊息
+    $lang->msg_not_exists_addr = "找不到地址";
+    $lang->msg_fail_to_socket_open = "無法連結郵政編號伺服器";
+    $lang->msg_no_result = "查無結果";
+    $lang->msg_no_yahoo_xy = "想要輸出的結果";
+
+    $lang->msg_no_apikey = "使用 Yahoo 地圖必須要有 Yahoo api key。\n請選擇完模組後再輸入 api key &gt; 所見即得編輯器 &gt; <a href=\"#\" onclick=\"popopen('./?module=editor&amp;act=dispEditorAdminSetupComponent&amp;component_name=yahoo_map','SetupComponent');return false;\">插入 Yahoo 地圖</a>";
+    
+?>
