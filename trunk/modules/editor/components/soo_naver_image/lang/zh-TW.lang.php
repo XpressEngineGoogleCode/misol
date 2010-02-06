0a8351ef7a3986fba718a739a5cb8d47db1bcb00
royallin11 <royallin11@d0f3c0e8-0e7a-11df-96d9-85c6b80b8d9c>
2010-02-06 13:37:06 +0000

    Add TW language

---
+++ trunk/modules/editor/components/soo_naver_image/lang/zh-TW.lang.php
@@ -0,0 +1,32 @@
+<?php
+    /**
+     * @file   /modules/editor/components/soo_naver_image/lang/ko.lang.php
+     * @author 민수 <misol221@paran.com> 翻譯：royallin
+     * @brief  編輯器模組 > Naver圖片 (soo_naver_image) 正體中文語言
+     **/
+
+    // 訊息
+    $lang->about_soo_image_search_sort = "排列順序";
+    $lang->about_soo_image_search_sort_sim = "類似";
+    $lang->about_soo_image_search_sort_date = "日期";
+    $lang->about_soo_image_search_filter = "圖片大小";
+    $lang->about_soo_image_search_filter_all = "所有圖片";
+    $lang->about_soo_image_search_filter_large = "大圖片";
+    $lang->about_soo_image_search_filter_midium = "中圖片";
+    $lang->about_soo_image_search_filter_small = "小圖片";
+
+    $lang->soo_msg_total = "總共";
+    $lang->soo_msg_result_num = "搜尋結果";
+    $lang->soo_msg_beforepage = "上一頁";
+    $lang->soo_msg_nextpage = "下一頁";
+    $lang->about_soo_image_search = "例) naver, 圖片, misol";
+    $lang->about_soo_image_result_info = "Naver搜尋 <img alt=\"Powered by NAVER OpenAPI\" src=\"http://openapi.naver.com/logo/logo07_1.gif\" width=\"135\" height=\"5\" />";
+    $lang->about_soo_image_use = "請點擊想搜尋的圖片";
+
+    // 錯誤訊息
+    $lang->msg_not_exists_addr = "找不到結果";
+    $lang->msg_fail_to_socket_open = "連結失敗";
+    $lang->msg_no_result = "找不到結果";
+
+    $lang->soo_image_msg_no_apikey = "請輸入 api key\n請到管理員頁面 > 網頁編輯器 > <a href=\"#\" onclick=\"popopen('./?module=editor&amp;act=dispEditorAdminSetupComponent&amp;component_name=soo_naver_image','SetupComponent');return false;\">Naver圖片</a> 輸入 api key";
+?>
