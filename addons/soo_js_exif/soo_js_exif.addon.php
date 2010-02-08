bd8e8626c6c3bf8185475e4bd36ba41cfa2b25d9
misol221 <misol221@paran.com>
2010-02-08 18:26:07 +0000

    brief information fix.

--- addons/soo_js_exif/soo_js_exif.addon.php
+++ addons/soo_js_exif/soo_js_exif.addon.php
@@ -2,7 +2,7 @@
 if(!defined('__ZBXE__')) exit();
 // file soo_js_exif.addon.php
 // author misol (misol@korea.ac.kr)
-// brief 자바스크립트를 이용해 이미지의 Exif 정보를 보여줍니다. Show Exif information for the images in a document on XE with only javascript.
+// brief 자바스크립트를 이용해 이미지의 Exif 정보를 보여줍니다. Show Exif information of the images in a document on XE with only javascript.
 // ⓒ 2010 김민수.
 // This addon uses MIT License based librarys, Binary Ajax 0.1.5, EXIF Reader 0.1.2, and ImageInfo 0.1.2, which is written by Jacob Seidelin.
 
