2f86097d08dfc8a667c105d958a9b1178ac1328f
misol221 <misol221@paran.com>
2010-06-15 14:48:45 +0000

    addon update

--- addons/soo_for_muzik_player/soo_for_muzik_player.addon.php
+++ addons/soo_for_muzik_player/soo_for_muzik_player.addon.php
@@ -9,6 +9,7 @@ if(Context::get('module') == 'admin') return;
 if(class_exists('smartphoneXE')) {
   if(smartphoneXE::isFromSmartPhone()) return;
 }
+$nac = 'soo_for_muzik_player';
 if($called_position == 'before_module_init') {
   if(Context::get('outframe')) {
     $_SESSION['soo_for_muzik_player']['outframe'] = false;
@@ -25,42 +26,34 @@ if(function_exists('isCrawler')) {
   if(isCrawler()) return;
 }
 
-if(Context::getResponseMethod()=='XMLRPC' && Context::get('module') == 'addon' && Context::get('act') == 'soo_for_muzik_player') {
+if(Context::getResponseMethod()=='XMLRPC' && Context::get('module') == 'addon' && Context::get('act') == 'soo_for_muzik_player' && !Context::get('soo_m_output')) {
   if(Context::get('do') == 'doFrameSessionStart') {
     $_SESSION['soo_for_muzik_player']['doFrameSessionStart'] = true;
-    header("Content-Type: text/xml; charset=UTF-8");
-    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-    header("Cache-Control: no-store, no-cache, must-revalidate");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
-    $xmlDoc  = "<response>\n<error>0</error>\n<message>success</message>\n</response>";
-    echo $xmlDoc;
-    exit();
+    $output_xml  = "<response>\n<error>0</error>\n<message>success</message>\n</response>";
+    Context::set('soo_m_output',$output_xml);
   }
   if(Context::get('do') == 'doFrameSessionChecker') {
-    header("Content-Type: text/xml; charset=UTF-8");
-    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-    header("Cache-Control: no-store, no-cache, must-revalidate");
-    header("Cache-Control: post-check=0, pre-check=0", false);
-    header("Pragma: no-cache");
     if($_SESSION['soo_for_muzik_player']['doFrameSessionStart']) {
-      $xmlDoc  = "<response>\n<error>0</error>\n<message>FrameSessionStartSuccess</message>\n</response>";
-      echo $xmlDoc;
-      exit();
+      $output_xml  = "<response>\n<error>0</error>\n<message>FrameSessionStartSuccess</message>\n</response>";
+      Context::set('soo_m_output',$output_xml);
     }
     else {
-      $xmlDoc  = "<response>\n<error>0</error>\n<message>FrameSessionStartFail</message>\n</response>";
-      echo $xmlDoc;
-      exit();
+      $output_xml  = "<response>\n<error>0</error>\n<message>FrameSessionStartFail</message>\n</response>";
+      Context::set('soo_m_output',$output_xml);
     }
   }
 }
-
+if($called_position == 'before_display_content' && Context::getResponseMethod() == 'XMLRPC') {
+  if(Context::get('soo_m_output')) {
+    $output = Context::get('soo_m_output');
+  }
+}
 if(Context::getResponseMethod() == 'HTML') {
+  ${$nac} = 'so';
+  ${$nac} .= 'o_fo';
   //POST 내용 있으면 동작 안함.
   if(is_array($_POST)) if(count($_POST)) return;
+  if($soo_for_muzik_player != 'soo_fo') return;
   // 프레임 탈출 코드.
   if($called_position == 'before_module_init') {
     //세션이 동작하지 않는 User-Agent를 위한 처리
@@ -104,7 +97,10 @@ if(Context::getResponseMethod() == 'HTML') {
         $attrbt .= 'scrolling="no" ';
       }
 
-      if(!$addon_info->frame_position || $addon_info->frame_position == 'up') $size = $addon_info->frame_size.',*';
+      if($addon_info->frame_position == 'left' || $addon_info->frame_position == 'right') $frame_pos = 'cols';
+      else $frame_pos = 'rows';
+
+      if(!$addon_info->frame_position || $addon_info->frame_position == 'up' || $addon_info->frame_position == 'left') $size = $addon_info->frame_size.',*';
       else $size = '*,'.$addon_info->frame_size;
 
       $langtype = Context::getLangType();
@@ -126,18 +122,18 @@ if(Context::getResponseMethod() == 'HTML') {
     }
   }
   '.'/'.'/]]></script>
-  </head> <frameset rows="'.$size.'" onload="framelct()">';
-      if(!$addon_info->frame_position || $addon_info->frame_position == 'up') {
+  </head> <frameset '.$frame_pos.'="'.$size.'" onload="framelct()">';
+      if(!$addon_info->frame_position || $addon_info->frame_position == 'up' || $addon_info->frame_position == 'left') {
         $output .= sprintf(' <frame id="new_frame" name="new_frame" src="%s" %s />', $addon_info->frame_page, $attrbt);
       }
 
       $output .= sprintf(' <frame id="contents_frame" name="contents_frame" src="%s" frameborder="%s" scrolling="auto" />', $uri, $addon_info->frame_border);
 
-      if($addon_info->frame_position == 'down') {
+      if($addon_info->frame_position == 'down' || $addon_info->frame_position == 'right') {
         $output .= sprintf(' <frame id="new_frame" name="new_frame" src="%s" %s />', $addon_info->frame_page, $attrbt);
       }
       $output .= "\n";
-      $output .= '<noframes><body> <p>죄송합니다. 당신의 브라우저는 프레임셋을 지원하지 않습니다.<br />Sorry, your browser does not handle frames!</p> <p>프레임 셋을 제거하고 페이지를 계속 보시려면 아래 링크를 클릭해주세요.<br />If you want to see this site without frames, click the following link.<br />';
+      $output .= '<noframes><body> <p>죄송합니다. 이 브라우저는 프레임셋을 지원하지 않습니다.<br />Sorry, your browser does not handle frames!</p> <p>프레임 셋을 제거하고 페이지를 계속 보시려면 아래 링크를 클릭해주세요.<br />If you want to see this site without frames, click the following link.<br />';
       $output .= ' <a href="'.$exit_uri.'">'.$exit_uri.'</a></p> </body> </noframes> </frameset> </html>';
       // 변경된 문서가 헤더를 제대로 달고 나가고, 다른 애드온들에 의해 HTML편집 당하는 일 없게 JSON방식인것처럼 속이기(?!) 이런 방식으로 출력하는 것이 아니었으나... 어떤 홈페이지에서 작동 안되는 것을 발견하여서... 하다보니.. 이 방법 외엔 없었다 ㅜ.ㅜ;
       Context::setResponseMethod('JSON');
