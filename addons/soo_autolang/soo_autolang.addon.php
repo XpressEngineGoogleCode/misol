ed8bb7ebe657a912710af2016aa63c7f02ebd79d
misol.kr <misol.kr@d0f3c0e8-0e7a-11df-96d9-85c6b80b8d9c>
2011-01-25 06:32:25 +0000

    autolang addon bug fixed.

--- addons/soo_autolang/soo_autolang.addon.php
+++ addons/soo_autolang/soo_autolang.addon.php
@@ -9,26 +9,60 @@ if(!defined("__ZBXE__")) exit();
 if($_COOKIE['lang_type']) return;
 if($called_position != 'before_module_init' || Context::get('module')=='admin') return;
 
-// 언어-지역 코드 변환용 배열. XE 언어 코드와 같은 것은 제외. 파이어폭스 브라우저에서 추출;; (인터넷은 뒤져도 못찾음 ㅠ)
+// 언어-지역 코드 변환용 배열. XE 언어 코드와 같은 것은 제외. 참고 : http://forums.mozilla.or.kr/viewtopic.php?p=33268
 $xe_langtype = array(
 	'ko','ko',
-	'en','en','en','en','en','en','en','en','en','en','en','en',
-	'zh-CN','zh-CN','zh-CN','zh-CN',
+
+	'en','en','en','en','en',
+	'en','en','en','en','en',
+	'en','en','en',
+
+	'zh-TW','zh-TW','zh-TW',
+
+	'zh-CN','zh-CN','zh-CN','zh-CN','zh-CN',
+
 	'jp','jp','jp','jp',
-	'es','es','es','es','es','es','es','es','es','es','es','es','es','es',
+
 	'es','es','es','es','es',
-	'fr','fr','fr','fr','fr','fr',
-	'zh-TW'
+	'es','es','es','es','es',
+	'es','es','es','es','es',
+	'es','es','es','es',
+
+	'fr','fr','fr','fr','fr',
+	'fr',
+
+	'ru',
+
+	'vi',
+
+	'mn'
 );
 $http_langtype = array(
 	'ko-kr','ko-kp',
-	'en-gb','en-us','en-au','en-ie','en-ca','en-nz','en-za','en-bz','en-jm','en-zw','en-tt','en-ph',
-	'zh','zh-cn','zh-sg','zh-hk',
-	'ja','ja-jp','ja-jp-mac','ja-jp-osaka',
-	'es-es','es-ar','es-mx','es-cl','es-gt','es-ni','es-do','es-ve','es-bo','es-ec','es-sv','es-hn','es-uy','es-cr',
-	'es-co','es-pa','es-py','es-pe','es-pr',
-	'fr-fr','fr-ca','fr-lu','fr-mc','fr-be','fr-ch',
-	'zh-tw'
+
+	'en-gb','en-us','en-au','en-ie','en-ca',
+	'en-nz','en-za','en-bz','en-jm','en-zw',
+	'en-tt','en-ph','en-cb',
+
+	'zh-tw','zh-hk','zh-cht',
+
+	'zh-cn','zh-sg','zh-chs','zh-mo','zh',
+
+	'ja-jp','ja-jp-mac','ja-jp-osaka','ja',
+
+	'es-es','es-ar','es-mx','es-cl','es-gt',
+	'es-ni','es-do','es-ve','es-bo','es-ec',
+	'es-sv','es-hn','es-uy','es-cr','es-co',
+	'es-pa','es-py','es-pe','es-pr',
+
+	'fr-fr','fr-ca','fr-lu','fr-mc','fr-be',
+	'fr-ch',
+
+	'ru-ru',
+
+	'vi-vn',
+
+	'mn-mn'
 );
 
 // 브라우저마다 대소문자는 다르므로 전부 소문자로 바꿔서 비교.
@@ -39,7 +73,7 @@ $lang_supported = Context::get('lang_supported');
 
 $http_user_lang = explode(',', $http_user_lang);
 
-if(is_array($lang_supported)) {
+if(is_array($lang_supported) && is_array($http_user_lang)) {
 	$auto_selected = -2;
 	foreach($http_user_lang as $user_key => $user_val) {
 		if($auto_selected == -1) break;
