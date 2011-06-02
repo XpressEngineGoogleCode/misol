763f772087dd992381dcd1fe28bc1f079ef08423
royallin11 <royallin11@d0f3c0e8-0e7a-11df-96d9-85c6b80b8d9c>
2011-06-02 13:24:28 +0000

    Added/Modified TW Translation.

---
+++ modules/editor/components/freeuse_license/lang/zh-TW.lang.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * @file   /modules/editor/components/freeuse_license/lang/zh-TW.lang.php
+     * @author 민수 <misol221@paran.com>
+     * @brief  編輯器(editor) 模組 >  資料共享許可組件
+     **/
+
+    $lang->freeuse_default_title = '資料共享許可';
+    $lang->freeuse_default_message = '이 문서는 <a rel="license" href="http://freeuse.or.kr/license/%s/%s%s%s" target="_blank"> %s %s :%s%s</a>을(를) 따릅니다.';
+
+    $lang->freeuse_title = '標題';
+    $lang->freeuse_use_mark = '標誌';
+    $lang->freeuse_allow_commercial = '允許營利';
+    $lang->freeuse_allow_modification = '允許修改';
+    $lang->freeuse_author = '作者';
+
+    $lang->freeuse_allow = '允許';
+    $lang->freeuse_disallow = '禁止';
+
+    $lang->freeuse_options = array(
+        'freeuse_allow_commercial' => array('Y'=>'', 'N'=>' 禁止營利'),
+        'freeuse_allow_modification' => array('Y'=>'', 'N'=>' 禁止修改'),
+    );
+
+    $lang->about_freeuse_title = '顯示標題。 留白將會顯示預設標題。';
+    $lang->about_freeuse_use_mark = '可選擇是否顯示標誌。 (預設: 顯示)';
+    $lang->about_freeuse_allow_commercial = '可選擇是否允許商業用途。 (預設: 禁止)';
+    $lang->about_freeuse_allow_modification = '可選擇是否允許修改文件。(預設: 禁止)';
+    $lang->about_freeuse_author = '請輸入文件作者。 (必填)';
+?>
